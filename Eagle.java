b79a43ee468a4b0a3bc2c200db5b166289be9961
matgoy <gohiermathieu78@gmail.com>
2022-10-25 12:24:44 +0200

    solution

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,37 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
+
+
+
+    @Override
+    public void takeOff(){
+        System.out.println("son envol");
+    }
+
+    @Override
+    public int ascend(int meters){
+
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.printf("%s ascend upward, alt : %d%n", this.getName(), this.altitude);
+
+        return this.altitude;
+    }
+    @Override
+    public int descend(int meters){
+
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s descend downward, alt : %d%n", this.getName(), this.altitude);
+
+        return this.altitude;
+    }
+    @Override
+    public void land(){
+        System.out.println("france");
+    }
+
 }
